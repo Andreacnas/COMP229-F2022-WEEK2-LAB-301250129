5c6dc4603ba388f0688684ec3047adb5ddb601f9
Andrea Cavalcanti <67080323+Andreacnas@users.noreply.github.com>
2022-09-18 16:16:16 -0400

    Update server js with third-party nodejs module Connect + add custom middleware text/html

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-//Import http-web-server nodejs core package
-const http = require('http');
+//Import third-party nodejs module "Connect"
+const connect = require('connect');
 
-//Create the webserver event handler
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello from NodeJS Application');
-});
+//Instatiate app-server
+const app = connect();
 
-server.listen(3000);
+//Custom middleware: text/plain
+function helloPlain(req,res,next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from NodeJS Application as html');
+}
+
+//Custom middleware: text/html
+function helloHTML(req,res,next){
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello from NodeJS Application as html</h1>');
+}
+
+//Add middleware to connect application
+app.use('/html',helloHTML);
+app.use('/',helloPlain);
+
+//Run app
+app.listen(3000);
 
 console.log('Server running at http://locahost:3000');
